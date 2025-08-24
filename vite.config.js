ad1c7039fd46ef33733f02e972d17400073f0b48
NarideHK <naridehk201901@gmail.com>
2025-08-24 12:59:44 +0000

    更新

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// 修改 base 為你的 GitHub repo 名稱（部署到 GitHub Pages 時必須設定）
 export default defineConfig({
   plugins: [react()],
-  base: '/naride-globalprocure/', 
+  // ⚠️ 這裡要改成你的 GitHub repo 名稱
+  base: '/Naride-globalprocure/',
 })
